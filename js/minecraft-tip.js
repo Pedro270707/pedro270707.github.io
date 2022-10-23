30f720e17430acbf3f92295ccd663d694885ef2a
Pedro270707 <pedro2007.prrr@gmail.com>
2022-10-23 17:10:55 -0300

    added §k

--- js/minecraft-tip.js
+++ js/minecraft-tip.js
@@ -1,10 +1,13 @@
-var textOutput = document.getElementById("minecraft-text-output");
 var textInput = document.getElementById("minecraft-text-input");
 var itemInputText = document.getElementById("item-input-text");
 var outputItem = document.getElementById('output-item');
 var mcTip = document.getElementById("minecraft-tip");
 var itemGlow = document.getElementById("item-glow");
 var obfuscatedTextManager = document.getElementById("k-manager");
+var tipTextLine = document.getElementById("tip-text-line");
+var tipShadowLine = document.getElementById("tip-shadow-line");
+var outputTextLine = document.getElementById("output-text-line");
+var outputShadowLine = document.getElementById("output-shadow-line");
 
 var breakCount = 0
 
@@ -16,7 +19,8 @@ function updateItem(event) {
 textInput.onkeyup = function() {
 	textOutputFormatted = textInput.value.replace(/</gi, "&lt;");
 	textOutputFormatted = textOutputFormatted.replace(/</g, "&gt;");
-	// textOutputFormatted = textOutputFormatted.replace(/&el/g, '<br><br>');
+	textOutputFormatted = textOutputFormatted.replace(/&br/g, '§r<br class="break">');
+	textOutputFormatted = textOutputFormatted.replace(/&el/g, '§r<br class="empty-line">');
 	textOutputFormatted = textOutputFormatted.replace(/§0/g, '</span><span class="c-0">');
 	textOutputFormatted = textOutputFormatted.replace(/§1/g, '</span><span class="c-1">');
 	textOutputFormatted = textOutputFormatted.replace(/§2/g, '</span><span class="c-2">');
@@ -41,25 +45,18 @@ textInput.onkeyup = function() {
 	textOutputFormatted = textOutputFormatted.replace(/§m/g, '<m class="c-m">');
 	textOutputFormatted = textOutputFormatted.replace(/§k/g, '<k class="c-k">');
 	textOutputFormatted = textOutputFormatted.replace(/§r/g, '</span></b></i></n></m></k>');
-	obfuscatedTextManager.innerHTML = textOutputFormatted.replace(/<k class="c-k">/g, '<k id="c-k-manager">');
-	var textOutputArray = textOutputFormatted.split("&br");
+	obfuscatedTextManager.innerHTML = textOutputFormatted.replace(/<k class="c-k">/g, '<k class="c-k-manager">');
 	
-	breakCount = textOutputArray.length - 1;
+	if (textOutputFormatted.match(/<br class="break">/g) != null) {
+		breakCount = textOutputFormatted.match(/<br class="break">/g).length;
+	} else {
+		breakCount = 0;
+	}
 
-	var textOutputReformatted = "";
-	textOutputArray.forEach(function(element, index, array) {
-		textOutputReformatted += "<p class='tip-line'>" + element + "</span></m></span></p><p class='tip-line shadow'>" + element + "</span></m></span></p>";
-		if (index + 1 != array.length) {
-			textOutputReformatted += "<br class='break'>";
-		}
-		if (textOutputReformatted == "<p class='tip-line'></span></m></span></p><p class='tip-line shadow'></span></m></span></p>") {
-			textOutput.innerHTML = "<p class='tip-line'>Nomes de itens do Minecraft</p><p class='tip-line shadow'>Nomes de itens do Minecraft</p>";
-			mcTip.innerHTML = "<p class='tip-line'>Nomes de itens do Minecraft</p><p class='tip-line shadow'>Nomes de itens do Minecraft</p>";
-		} else {
-			textOutput.innerHTML = textOutputReformatted;
-			mcTip.innerHTML = textOutputReformatted;
-		}
-	});
+	tipTextLine.innerHTML = textOutputFormatted;
+	tipShadowLine.innerHTML = textOutputFormatted;
+	outputTextLine.innerHTML = textOutputFormatted;
+	outputShadowLine.innerHTML = textOutputFormatted;
 }
 
 $('.slot-item').mouseover(function(event) {
@@ -119,56 +116,28 @@ $(".minecraft-item").mouseout(function(event) {
   $(mcTip).css("display", "none");
 });
 
-/* setInterval(function() {
+setInterval(function() {
   randomizeText();
 }, 50);
 
 function randomizeText(obftext) {
-		for (var group = 0; group < document.querySelectorAll("k#c-k-manager").length; group++) {
-			var length = document.querySelectorAll("k#c-k-manager")[group].textContent.length;
-			var result = "";
-			var largeCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyzÀÁÂÈÊËÍÓÔÕÚßãõğİıŒœŞşŴŵžȇ#$%&"*+-/0123456789<=>?@[\]^_`{}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑªº¿®¬½¼«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αβΓπΣσμτΦΘΩδ∞∅∈∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■';
-			var smallCharacters = "i'!,.:;|¡";
+	for (var group = 0; group < document.getElementsByClassName("c-k-manager").length; group++) {
+		var length = document.getElementsByClassName("c-k-manager")[group].textContent.length;
+		var result = "";
+		var largeCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghjkmnopqrstuvwxyzÀÁÂÈÊËÍÓÔÕÚßãõğİıŒœŞşŴŵžȇ#$%&"*+-/0123456789<=>?@[\]^_`{}~ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜø£Ø×ƒáíóúñÑªº¿®¬½¼«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αβΓπΣσμτΦΘΩδ∞∅∈∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■';
+		var smallCharacters = "i'!,.:;|¡";
 			
-			for (var i = 0; i < length; i++) {
-				if (smallCharacters.includes(document.querySelectorAll("k#c-k-manager")[group].textContent.charAt(i))) {
-					result += smallCharacters.charAt(Math.floor(Math.random() * 9));
-				} else {
-					result += largeCharacters.charAt(Math.floor(Math.random() * 246));
-				}
-			}
-			
-		//if(document.querySelectorAll("k.c-k")[group].firstChild.nodeName == '#text') {
-		//	for (i = 0; i < document.getElementsByClassName("tip-line").length; i++) {
-		//		document.querySelectorAll("k.c-k")[group].firstChild.textContent = result;
-		//		document.querySelectorAll(".shadow")[group].firstChild.firstChild.textContent = result;
-		//	}
-		//} else if(document.querySelectorAll("k.c-k")[group].firstChild.firstChild.nodeName == '#text') {
-		//	for (i = 0; i < document.getElementsByClassName("tip-line").length; i++) {
-		//		document.querySelectorAll("k.c-k")[group].firstChild.firstChild.textContent = result;
-		//		document.querySelectorAll(".shadow")[group].firstChild.firstChild.textContent = result;
-		//	}
-		// }
-		
-		for (var i = 1; i <= document.querySelectorAll("k#c-k-manager").length * 4; i++) {
-			if(document.querySelectorAll("k.c-k")[group].firstChild.nodeName == '#text') {
-				document.querySelectorAll("k.c-k")[group].firstChild.textContent = result;
-				document.querySelectorAll("k.c-k")[group + document.querySelectorAll("k#c-k-manager").length].firstChild.textContent = result;
-				document.querySelectorAll("k.c-k")[group + document.querySelectorAll("k#c-k-manager").length * 2].firstChild.textContent = result;
-				document.querySelectorAll("k.c-k")[group + document.querySelectorAll("k#c-k-manager").length * 3].firstChild.textContent = result;
-			}
-			if(document.querySelectorAll("k.c-k")[group].children != ) {
-				document.querySelectorAll("k.c-k")[group].firstChild.textContent = result;
-				document.querySelectorAll("k.c-k")[group + document.querySelectorAll("k#c-k-manager").length].firstChild.firstChild.textContent = result;
-				document.querySelectorAll("k.c-k")[group + document.querySelectorAll("k#c-k-manager").length * 2].firstChild.firstChild.textContent = result;
-				document.querySelectorAll("k.c-k")[group + document.querySelectorAll("k#c-k-manager").length * 3].firstChild.firstChild.textContent = result;
+		for (var i = 0; i < length; i++) {
+			if (smallCharacters.includes(document.getElementsByClassName("c-k-manager")[group].textContent.charAt(i))) {
+				result += smallCharacters.charAt(Math.floor(Math.random() * 9));
+			} else {
+				result += largeCharacters.charAt(Math.floor(Math.random() * 246));
 			}
 		}
 		
-		// if(document.querySelectorAll("k.c-k")[group].firstChild.nodeName == '#text') {
-			// document.querySelectorAll("k.c-k")[group].firstChild.textContent = result;
-		// } else if(document.querySelectorAll("k.c-k")[group].firstChild.firstChild.nodeName == '#text') {
-			// document.querySelectorAll("k.c-k")[group].firstChild.firstChild.textContent = result;
-		// }
+		tipTextLine.getElementsByClassName("c-k")[group].innerHTML = result;
+		tipShadowLine.getElementsByClassName("c-k")[group].innerHTML = result;
+		outputTextLine.getElementsByClassName("c-k")[group].innerHTML = result;
+		outputShadowLine.getElementsByClassName("c-k")[group].innerHTML = result;
 	}
-} */
+}
