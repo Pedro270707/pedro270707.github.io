28c9dfa4445bf45b57763cf97211054e32a03370
Pedro270707 <pedro2007.prrr@gmail.com>
2022-10-25 23:30:49 -0300

    added favo(u)rite items

--- js/terraria-tip.js
+++ js/terraria-tip.js
@@ -1,11 +1,17 @@
 const textInput = document.getElementById("terraria-text-input");
 const itemInputText = document.getElementById("item-input-text");
+const itemOutput = document.getElementById("item-output");
 const outputItem = document.getElementById("output-item");
 const terrariaTip = document.getElementById("terraria-tip");
 const tipTextLine = document.getElementById("tip-text-line");
 const tipShadowLine = document.getElementById("tip-shadow-line");
 const outputTextLine = document.getElementById("output-text-line");
 const outputShadowLine = document.getElementById("output-shadow-line");
+const itemCount = document.getElementById("item-count");
+const itemCountInput = document.getElementById("item-count-input");
+
+var favoriteCursor = false;
+var favoriteSlot = false;
 
 function updateItem(event) {
 	itemInputText.innerHTML = "Alterar imagem do item";
@@ -13,8 +19,8 @@ function updateItem(event) {
 }
 
 textInput.onkeyup = function() {
-	textOutputFormatted = textInput.value.replace(/</gi, "&lt;");
-	textOutputFormatted = textOutputFormatted.replace(/</g, "&gt;");
+	textOutputFormatted = textInput.value.replace(/</g, "&lt;");
+	textOutputFormatted = textOutputFormatted.replace(/>/g, "&gt;");
 	textOutputFormatted = textOutputFormatted.replace(/&br/g, '§r<br class="break">');
 	textOutputFormatted = textOutputFormatted.replace(/&el/g, '§r<br class="empty-line">');
 	textOutputFormatted = textOutputFormatted.replace(/&nbsp/g, '§r<div class="no-break-space"></div>');
@@ -48,6 +54,10 @@ textInput.onkeyup = function() {
 		outputTextLine.innerHTML = textOutputFormatted;
 		outputShadowLine.innerHTML = textOutputFormatted;
 	}
+}
+
+itemCountInput.onkeyup = function() {
+	itemCount.innerHTML = itemCountInput.value.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 }
 
 $('.slot-item').mouseover(function(event) {
@@ -87,3 +97,29 @@ $(".terraria-item").mousemove(function(event) {
 $(".terraria-item").mouseout(function(event) {
   $(terrariaTip).css("display", "none");
 });
+
+document.addEventListener("keydown", function(event) {
+    if (event.key == "Control"){
+        itemOutput.classList.add("favorite-cursor");
+		favoriteCursor = true;
+	}
+});
+
+document.addEventListener("keyup", function(event) {
+    if (event.key == "Control"){
+        itemOutput.classList.remove("favorite-cursor");
+		favoriteCursor = false;
+	}
+});
+
+itemOutput.onclick = function() {
+	if (favoriteCursor == true) {
+		if (favoriteSlot == true) {
+			itemOutput.classList.remove("favorite");
+			favoriteSlot = false;
+		} else {
+			itemOutput.classList.add("favorite");
+			favoriteSlot = true;
+		}
+	}
+}
