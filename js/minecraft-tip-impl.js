fefb2a3c85ea8dcd67e6051b4303b457dea462a3
Pedro <pedro2007.prrr@gmail.com>
2024-03-09 16:51:45 -0300

    Saved tooltips now have a preview on hover

--- js/minecraft-tip-impl.js
+++ js/minecraft-tip-impl.js
@@ -10,25 +10,29 @@ new TranslatableText("minecrafttooltips-defaulttooltip").get().then(str => {
     document.getElementById("tooltip-output").appendChild(tooltip);
 });
 
-const followerTooltip = document.body.appendChild(createTooltip("Follower tooltip", true));
+const followerTooltip = createTooltip("Follower tooltip", true);
 followerTooltip.classList.add("hidden");
 const minecraftItems = document.getElementsByClassName("minecraft-item");
 
 for (let i = 0; i < minecraftItems.length; i++) {
-    minecraftItems.item(i).addEventListener('mouseenter', (e) => {
-        followerTooltip.classList.remove("hidden");
+  minecraftItems.item(i).addEventListener('mouseenter', (e) => {
+    if (!e.target.dataset.mctitle) return;
+    followerTooltip.classList.remove("hidden");
+    getTextFromJSON(e.target.dataset.mctitle).get().then(str => {
+      setTooltipText(followerTooltip, str);
     });
+  });
 
-    minecraftItems.item(i).addEventListener('mouseleave', (e) => {
-        followerTooltip.classList.add("hidden");
-    });
+  minecraftItems.item(i).addEventListener('mouseleave', (e) => {
+    followerTooltip.classList.add("hidden");
+  });
 }
+document.body.appendChild(followerTooltip);
 
 setInterval(() => {
     if (tooltipField.value === '') {
         new TranslatableText("minecrafttooltips-defaulttooltip").get().then(str => {
             setTooltipText(tooltip, str);
-            setTooltipText(followerTooltip, str);
         });
     } else {
         if (tooltipField.value.startsWith("style:") && styles[tooltipField.value.substring("style:".length)]) {
@@ -41,7 +45,6 @@ setInterval(() => {
         }
         const unescapedValue = tooltipField.value.replace(/\\n/g, "\n").replace(/\\\n/g, "\\n");
         setTooltipText(tooltip, unescapedValue);
-        setTooltipText(followerTooltip, unescapedValue);
     }
 }, 50);
 
@@ -156,6 +159,18 @@ function updateSavedTooltips() {
             tooltipText.addEventListener("click", (e) => {
                 tooltipField.value = tooltipTextInner.textContent;
             })
+            tooltipText.dataset.mctitle = element.replace(/\\n/g, "\n").replace(/\\\n/g, "\\n");
+
+            tooltipText.addEventListener('mouseenter', (e) => {
+                if (!e.target.dataset.mctitle) return;
+                followerTooltip.classList.remove("hidden");
+                setTooltipText(followerTooltip, tooltipText.dataset.mctitle);
+            });
+            
+            tooltipText.addEventListener('mouseleave', (e) => {
+                followerTooltip.classList.add("hidden");
+            });
+
             domElement.appendChild(tooltipText);
             let removeTooltip = document.createElement("button");
             removeTooltip.classList.add("icon-button");
