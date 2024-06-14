4d7be921a87451ab4878c7755aabac427726321a
Pedro <pedro2007.prrr@gmail.com>
2024-06-14 00:18:26 -0300

    Attempt to make dynamic tooltips

---
+++ netlify/functions/minecrafttooltips.ts
@@ -0,0 +1,23 @@
+import { createCanvas } from 'canvas';
+
+const generateTooltipImage = (req, res) => {
+    const canvas = createCanvas(16, 16).getContext('2d').canvas;
+    for (const key in req.query) {
+        if (Object.hasOwnProperty.call(req.query, key)) {
+            const value = req.query[key];
+            
+            canvas.setAttribute("data-setting-" + key, value);
+        }
+    }
+
+    drawTooltip(canvas, new TextRenderer(), false);
+
+    const canvasData = canvas.toDataURL();
+
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Cache-Control', 'no-store');
+
+    res.send(canvasData);
+};
+
+export default generateTooltipImage;
